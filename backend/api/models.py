b01b2c7a772a3ebcc42b72bd769e4fd538b1ff9c
John R. O'Donnell <matadorcrab@gmail.com>
2023-08-10 13:38:30 -0500

    registered users

--- backend/api/models.py
+++ backend/api/models.py
@@ -7,5 +7,5 @@ class Note(models.Model):
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return self.body()
+    # def __str__(self):
+    #     return self.body(0,50)
