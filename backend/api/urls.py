1662b17ce68437b713b7e46a3f09c33b4dd9cb5f
John R. O'Donnell <matadorcrab@gmail.com>
2023-08-10 13:54:30 -0500

    serialized the route

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('',views.getRoutes, name="routes"),
     path('notes/', views.get_notes, name="notes")
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .models import Note
+from .serializers import NoteSerializer
 
 # Create your views here.
 
@@ -10,4 +12,7 @@ def getRoutes(request):
 
 @api_view(['GET'])
 def get_notes(request):
-    return Response('Notes')
+    notes = Note.objects.all()
+    serializer = NoteSerializer(notes, many=True)
+
+    return Response(serializer.data)
