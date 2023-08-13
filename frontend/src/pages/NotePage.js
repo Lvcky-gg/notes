19ed67b67cc44dc19697ac916e0860d62a7d82f9
John R. O'Donnell <matadorcrab@gmail.com>
2023-08-13 14:45:31 -0500

    init state for single

--- frontend/src/pages/NotePage.js
+++ frontend/src/pages/NotePage.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom'
+import { useDispatch} from 'react-redux';
+import { getANote } from '../store/notes';
+
 
 const NotePage = () => {
+  const {noteId }= useParams();
+  const dispatch = useDispatch();
+  const note = useSelector((state)=> state.notes.allNotes);
+  useEffect(()=>{
+    dispatch(getANote(noteId))
+
+  },[dispatch])
+
+  
   return (
     <div>
-      <h1>Single Note</h1>
+      <h1>{note.body}</h1>
     </div>
   )
 }
--- frontend/src/store/notes.js
+++ frontend/src/store/notes.js
@@ -23,6 +23,16 @@ export const noteSlice = createSlice({
             state.errors = action.payload
 
         })
+        .addCase(getANote.fulfilled, (state, action)=>{
+            state.allNotes = action.payload
+            state.errors = null
+
+        })
+        .addCase(getANote.rejected, (state, action)=>{
+            state.allNotes = null
+            state.errors = action.payload
+
+        })
 
     }
 })
@@ -33,6 +43,25 @@ export const getAllNotes = createAsyncThunk(
         try {
             const res = await axios.get(
                 "/api/notes",
+                   {
+                    headers: {
+                        "Content-Type": "application/json",
+                        "XSRF-Token": Cookies.get("XSRF-TOKEN"),
+                      },
+                   });
+            if(res.data)return res.data
+        }catch(err){
+            return rejectWithValue({errors:err.response.data.errors})
+        }
+    }
+)
+
+export const getANote = createAsyncThunk(
+    "notes/getANote",
+    async (noteId, {rejectWithValue}) => {
+        try {
+            const res = await axios.get(
+                `/api/notes/${noteId}`,
                    {
                     headers: {
                         "Content-Type": "application/json",
